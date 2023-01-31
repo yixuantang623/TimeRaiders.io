031f592a6fa66c14506ca0833f047fa143d44e94
yixuantang623 <104302766+yixuantang623@users.noreply.github.com>
2023-01-31 11:20:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var s = skrollr.init() 
+
+
+$(document).on('click','a.item',function(evt){
+  var target = $(this).attr("href")
+  $('html,body').animate({
+    scrollTop: $(target).offset().top - 30
+  },500)
+})
+
+
+
+$(".eximg").click(function() {
+  $("#img").fadeIn("speed")
+ 
+});
+
+$(".btnx").click(function() {
+  closeWindow();
+});
+function closeWindow(){
+  $("#img").fadeOut(200);
+  $(".dialouge").removeClass("transform-in").addClass("transform-out");
+  event.preventDefault();
+}
+
+
+$(document).ready(function(){
+  $('.showmenu').on('click', function(e){
+    e.preventDefault();
+    $('body').toggleClass('menu-show');
+  })
+})
+
+$(document).ready(function(){
+  $('.item').on('click', function(e){
+    e.preventDefault();
+    $('body').toggleClass('menu-show');
+  })
+})
+
+
+$(".eximg").click(function() {
+  $("#img").fadeIn("speed")
+ 
+});
+
+$(".popup-close").click(function() {
+  closeWindow();
+});
+function closeWindow(){
+  $("#img").fadeOut(200);
+  $("#text").fadeOut(200);
+  $(".dialouge").removeClass("transform-in").addClass("transform-out");
+  $(".box").removeClass("transform-in").addClass("transform-out");
+  event.preventDefault();
+}
+
+
+
+
+$("#winner").click(function() {
+  $("#text").fadeIn("speed")
+ 
+});
